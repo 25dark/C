688c5193eb9d36bd14529c802969039ec16ebbd1
25dark <henrydannso6@gmail.com>
2022-09-07 18:17:25 +0000

    added a scanf page

--- scan.c
+++ scan.c
@@ -6,8 +6,15 @@ Date: 07|09|22
 */
 
 #include <stdio.h>
-int main() {
 
-    printf("Hey, I'm learning C programming");
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter a value");
+
+    scanf("%d", &i);
+
+    printf("\nYou entered : %d\n", i);
     return 0;
 }
