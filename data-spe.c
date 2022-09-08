4ce94d929d8a87e60f41b44781bc23c781594d5e
25dark <henrydannso6@gmail.com>
2022-09-08 05:15:55 +0000

    updated with data spec file

---
+++ data-spe.c
@@ -0,0 +1,18 @@
+/*
+Author: Henry Danso;
+Purpose: Data specifying
+Date: 08|09|22
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    //variable with lot decimals
+    float x = 3.9923454;
+
+    printf("%.2f", x);
+    return 0;
+    //.2 means after the whole number
+    //print two decimal numbers (3.99)
+}
