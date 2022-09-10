f8ceb5365918855c19ded83db466755900d7f68e
25dark <henrydannso6@gmail.com>
2022-09-10 21:27:52 +0000

    enum challege

---
+++ challenges/enum-chal.c
@@ -0,0 +1,20 @@
+/* Author: Henry Danso
+   Purpose: Enum Challenge
+   Date: 10|09|22
+*/
+
+#include <stdio.h>>
+int main()
+{
+    //declare your enum variables
+    enum Company {GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT};
+    //declare the variables stated in the question to show values
+    enum Company xerox = XEROX;
+    enum Company google = GOOGLE;
+    enum Company ebay = EBAY;
+
+    printf("The Value of xerox is: %d\n", xerox);
+    printf("The Value of google is: %d\n", google);
+    printf("The Value of ebay is: %d\n", ebay);
+    return (0);
+}
